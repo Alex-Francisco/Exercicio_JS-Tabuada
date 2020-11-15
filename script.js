02749aebcca2ffb6946dcc7ac9646992ef720d4a
Alex Francisco <alex2007f@gmail.com>
2020-11-15 01:02:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,36 @@
 function gerar() {
     var num = document.querySelector('#num').value
     var inputNum = document.querySelector('#num')
-
-    if (inputNum.disabled) {
-        document.location.reload()
+    corpoTbl = document.querySelector('#corpoTbl')
+   
+    if (num == "") {
+        alert("Você deve inserir um número")
     } else {
-        if (num == "") {
-            alert("Você deve inserir um número")
-        } else {
-            for (var m = 0; m <= 10; m++) {
-                corpoTbl = document.querySelector('#corpoTbl')
+        corpoTbl.innerHTML = ""
+        for (var m = 0; m <= 10; m++) {        
+            trRecebeTd = document.createElement('tr')
+            trRecebeTd.setAttribute('class', 'teste')
+            tdNumTab = document.createElement('td')
+            corpoTbl.appendChild(trRecebeTd)
+            trRecebeTd.appendChild(tdNumTab)
+            tdNumTab.innerHTML += num + "<br>"
+              
+            tdSinalOp = document.createElement('td')
+            trRecebeTd.appendChild(tdSinalOp)
+            tdSinalOp.innerHTML += 'x'
                 
-                trRecebeTd = document.createElement('tr')
-                trRecebeTd.setAttribute('class', 'teste')
-                tdNumTab = document.createElement('td')
-                corpoTbl.appendChild(trRecebeTd)
-                trRecebeTd.appendChild(tdNumTab)
-                tdNumTab.innerHTML += num + "<br>"
-                
-                tdSinalOp = document.createElement('td')
-                trRecebeTd.appendChild(tdSinalOp)
-                tdSinalOp.innerHTML += 'x'
-                
-                tdMult = document.createElement('td')
-                trRecebeTd.appendChild(tdMult)
-                tdMult.innerHTML += m
+            tdMult = document.createElement('td')
+            trRecebeTd.appendChild(tdMult)
+            tdMult.innerHTML += m
     
-                tdSinalIgual = document.createElement('td')
-                trRecebeTd.appendChild(tdSinalIgual)
-                tdSinalIgual.innerHTML += '='
+            tdSinalIgual = document.createElement('td')
+            trRecebeTd.appendChild(tdSinalIgual)
+            tdSinalIgual.innerHTML += '='
     
-                tdResultado = document.createElement('td')
-                trRecebeTd.appendChild(tdResultado)
-                tdResultado.innerHTML += m * num 
-            }
-            btnMostrar = document.querySelector("#btnMostrar")
-            btnMostrar.innerHTML = "Resetar para digitar outra tabuada"
-            inputNum.disabled = true
+            tdResultado = document.createElement('td')
+            trRecebeTd.appendChild(tdResultado)
+            tdResultado.innerHTML += m * num 
         }
-    }
-    
+    }          
 }
+    
